7ddd4f90ea2474451f445b24e79a91bc18ec7ce5
RedSkull_7_7 <icaro.nata147@gmail.com>
2023-08-29 12:55:50 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/redskull7" />
-    <Titulo fontSize={20}>Icaro Natã Fadel</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Icaro Natã Fadel</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        RedSkull
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Full-Stack
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
