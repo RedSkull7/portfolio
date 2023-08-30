39dd908c40fb75f7094589fea836e1134ed46ab3
RedSkull_7_7 <icaro.nata147@gmail.com>
2023-08-30 07:23:37 -0300

    finalizando projeto

--- src/containers/Sidebar/styles.ts
+++ src/containers/Sidebar/styles.ts
@@ -10,9 +10,9 @@ export const BotaoTema = styled.button`
   font-weigth: bold;
   padding: 8px;
   border-radius: 12px;
-  color: #eee;
+  color: ${(props) => props.theme.corDeFundo};
   font-size: 10px;
-  background-color: #282a35;
+  background-color: ${(props) => props.theme.corPrincipal};
   cursor: pointer;
 `
 
@@ -20,4 +20,9 @@ export const SidebarContainer = styled.div`
   position: sticky;
   top: 80px;
   left: 0;
+
+  @media (max-width: 768px) {
+    margin-bottom: 40px;
+    text-align: center;
+  }
 `
